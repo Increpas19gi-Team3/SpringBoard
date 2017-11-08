e2392e71647cf87183f5a43fa5b313abaa4448f3
2-16 <2-16@192.168.0.40>
2017-11-08 17:14:20 +0900

    BoardDTO 검증 추가

--- src/dto/BoardDTO.java
+++ src/dto/BoardDTO.java
@@ -7,17 +7,30 @@ package dto;
 
 import java.sql.Date;
 
+import javax.validation.constraints.Max;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.sun.istack.internal.NotNull;
+
 public class BoardDTO {
 
 	private int NUM;			//게시글 번호, NUMBER(7)	NOT NULL, 시퀀스
 	private String ISNOTICE;	//공지글 여부, CHAR(1)	1-공지글, 0-일반글
 	private String ISBLOCK;		//블록 여부, CHAR(1)	1-블록, 0-일반글
+	@NotBlank(message="제목을 입력해주세요")
 	private String TITLE;		//제목,	VARCHAR2(50)
+	@NotBlank(message="작성자를 입력해주세요")
 	private String WRITER;		//작성자, VARCHAR2(20)
+	@NotBlank(message="비밀번호를 입력해주세요")
 	private String PWD;			//비밀번호, VARCHAR2(20)
+	@NotNull
+	@Max(value=3000,message="1500자 이하")
 	private String CONTENTS;	//글내용, VARCHAR2(3000)
 	private String IMGNAME;		//이미지파일명, VARCHAR2(500)
 	private int COUNT;			//조회수, NUMBER(4)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date REGTIME;		//작성일, DATE
 	
 	
